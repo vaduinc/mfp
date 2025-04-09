cf401e2a34b65711940aba5d96e94c14c89ad579
jaime valencia <jaime.valencia@sbtco.com>
2025-04-09 13:32:23 -0400

    trigger deployment

--- packages/marketing/src/components/Pricing.js
+++ packages/marketing/src/components/Pricing.js
@@ -18,7 +18,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website!
+        Your Website!3
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
