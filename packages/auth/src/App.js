fe954692670a87db48802b0e81d170474419bdf5
jaime valencia <jaime.valencia@sbtco.com>
2025-04-13 16:39:21 -0400

    add signin and out

---
+++ packages/auth/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import Singin from './components/Signin';
+import Signup from './components/Signup';
+
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'au',
+});
+
+export default ({ onSignIn, history }) => {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/auth/signin">
+              <Singin onSignIn={onSignIn} />
+            </Route>
+            <Route path="/auth/signup" component={Signup} >
+              <Singin onSignIn={onSignIn} />
+            </Route>
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+};
--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,8 +1,8 @@
-import { mount } from 'marketing/MarketingApp';
+import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -16,6 +16,9 @@ export default () => {
           history.push(nextPathname);
         }
       },
+      onSignIn: () => {
+        onSignIn();
+      }
     });
 
     history.listen(onParentNavigate);
